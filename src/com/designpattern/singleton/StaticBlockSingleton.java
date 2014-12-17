5d189288f328a8175153e55c667392dea89e304f
BHARAT SAVANI <savanibharat@gmail.com>
2014-12-16 23:51:59 -0800

    Static Block Singleton

---
+++ src/com/designpattern/singleton/StaticBlockSingleton.java
@@ -0,0 +1,18 @@
+package com.designpattern.singleton;
+
+public class StaticBlockSingleton {
+
+	private static StaticBlockSingleton instance;
+	private StaticBlockSingleton() {}
+	static{
+		try{
+			instance=new StaticBlockSingleton();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static StaticBlockSingleton getInstance(){
+		return instance;
+	}
+}
