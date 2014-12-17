755f14043bd4bf551933b09c63e2c57e7ff23c1d
BHARAT SAVANI <savanibharat@gmail.com>
2014-12-17 00:03:20 -0800

    Static Block Singleton Explanation

--- src/com/designpattern/singleton/StaticBlockSingleton.java
+++ src/com/designpattern/singleton/StaticBlockSingleton.java
@@ -1,18 +1,30 @@
 package com.designpattern.singleton;
 
+/**
+ * Static block initialization implementation is similar to eager
+ * initialization, except that instance of class is created in the static block
+ * that provides option for exception handling. Both eager initialization and
+ * static block initialization creates the instance even before it’s being used
+ * and that is not the best practice to use. So in further sections, we will
+ * learn how to create Singleton class that supports lazy initialization.
+ * 
+ * */
 public class StaticBlockSingleton {
 
 	private static StaticBlockSingleton instance;
-	private StaticBlockSingleton() {}
-	static{
-		try{
-			instance=new StaticBlockSingleton();
-		}
-		catch(Exception e){
+
+	private StaticBlockSingleton() {
+	}
+
+	static {
+		try {
+			instance = new StaticBlockSingleton();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public static StaticBlockSingleton getInstance(){
+
+	public static StaticBlockSingleton getInstance() {
 		return instance;
 	}
 }
