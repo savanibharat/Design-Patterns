7c0def6e5705856d34a8dbc7e9b2e6a59eeef131
BHARAT SAVANI <savanibharat@gmail.com>
2014-12-18 00:36:51 -0800

    Explanation

--- src/com/designpattern/singleton/LazyInitializationSingleton.java
+++ src/com/designpattern/singleton/LazyInitializationSingleton.java
@@ -1,13 +1,23 @@
 package com.designpattern.singleton;
 
+/**
+ * Lazy initialization method to implement Singleton pattern creates the
+ * instance in the global access method. The above implementation works fine
+ * in case of single threaded environment but when it comes to multi-threaded
+ * systems, it can cause issues if multiple threads are inside the if loop at
+ * the same time. It will destroy the singleton pattern and both threads will
+ * get the different instances of singleton class.
+ * */
 public class LazyInitializationSingleton {
 
 	private static LazyInitializationSingleton instance;
-	private LazyInitializationSingleton() {}
-	
-	public static LazyInitializationSingleton getInstance(){
-		if(instance==null){
-			instance=new LazyInitializationSingleton();
+
+	private LazyInitializationSingleton() {
+	}
+
+	public static LazyInitializationSingleton getInstance() {
+		if (instance == null) {
+			instance = new LazyInitializationSingleton();
 		}
 		return instance;
 	}
